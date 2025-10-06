a5f8a819e3203f632802186ff7fe1e45bc8b4142
Nellark <nelakulati@gmail.com>
2025-10-06 13:20:47 +0200

    added my cv

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ProfileImage from "../assets/Profile.jpeg";
-import MyCV from "../assets/CV_Yonela.pdf";
+import MyCV from "../assets/Yonela-CV.pdf";
 import HeroBg from "../assets/profile4.jpeg"; 
 
 const Hero: React.FC = () => {
